71a8b136c6e4ac2d412aa0d2b4425e861bd4534d
James G <jamessgrant@hotmail.co.uk>
2019-06-26 16:42:06 +0100

    Ft: logout route

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { login } from './login'
+import { login } from './login';
+import logout from './logout';
 
 const router = express.Router();
 
 router.get('/', (req, res) => res.send('Welcome to EduGate!'));
 router.post('/login', login.post);
 router.get('/login', login.get);
+router.get('/logout', logout);
 
 export default router;
