fd33d138c326714bc96fbd9ade5ea0b443d09569
James G <jamessgrant@hotmail.co.uk>
2019-06-26 16:40:00 +0100

    Ft: add cookie session

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import winston from 'winston';
 import bodyParser from 'body-parser';
 import path from 'path';
+import cookieSession from 'cookie-session';
 
 import router from './controllers/index';
 
@@ -12,6 +13,12 @@ const port = process.env.PORT || 3000;
 
 const app = express()
 
+app.use(cookieSession({
+  name: 'session',
+  secret: process.env.SECRET,
+  maxAge: 60 * 60 * 1000,
+  httpOnly: true,
+}))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
