c09c47587d4034c7b4ade09d24c8733374254c79
James G <jamessgrant@hotmail.co.uk>
2019-06-26 11:06:14 +0100

    Ft: add bodyParser, serve static files, add router

--- server/app.js
+++ server/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import winston from 'winston';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+import router from './controllers/index';
 
 dotenv.config();
 
@@ -8,7 +12,10 @@ const port = process.env.PORT || 3000;
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Welcome to EduGate!'))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(router);
 
 app.listen(port, () => winston.log("info",`Example app listening on port ${port}!`))
 
