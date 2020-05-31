63b6f18348101c7c00213f81d588906461127db6
sulenchy <sulaimanabiodun172@gmail.com>
2020-05-31 19:04:41 +0100

    remove mailgun credentials

--- server/helpers/sendEmail.js
+++ server/helpers/sendEmail.js
@@ -1,12 +1,9 @@
 import dotenv from 'dotenv';
 import mailgun from 'mailgun-js';
-const DOMAIN = 'sandbox1ac39a142ae44aee8b1a7d512b6f5e78.mailgun.org';
-
-const API_KEY = 'df4e1c458e2abc88e4b8ea3b83a6241e-7fba8a4e-a24b6a10';
-
-const mg = mailgun({ apiKey: API_KEY, domain: DOMAIN });
 
 dotenv.config()
+
+const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN });
 
 export default function sendEmail({ toAddress, subject, body }) {
   const data = {
